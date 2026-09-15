fa84b0365bdf33fab7237a3860f0ee4983996566
Jorge-Siqueira-Serrao <jorgefilhoz@hotmail.com>
2026-09-14 20:42:13 -0400

    aula 14/09/2026

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { HabitsProvider } from "./context/HabitsContext";
+import { StrictMode } from "react"; 
+import { createRoot } from "react-dom/client"; 
+import { BrowserRouter } from "react-router"; 
+import "./index.css"; import App from "./App"; 
+import { HabitsProvider } from "./context/HabitsContext"; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HabitsProvider> 
-      <App /> 
-    </HabitsProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render( 
+  <StrictMode> 
+    <BrowserRouter> 
+      <HabitsProvider> 
+        <App /> 
+      </HabitsProvider> 
+    </BrowserRouter> 
+  </StrictMode>, 
+);
